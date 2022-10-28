4ff9f4d77358af309edba06ff210f2cd2daa3dd0
LukasH0lm <lukasholm@hotmail.com>
2022-10-28 10:55:45 +0200

    initial commit

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+
+    private int id;
+    private String name;
+    private String email;
+
+
+    public Person(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
